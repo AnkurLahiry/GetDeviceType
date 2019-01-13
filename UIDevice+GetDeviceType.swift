5b6fd69eac48303ef344137473ed290bcc044198
ankur.lahiry <ankur.lahiry@ipay.com.bd>
2019-01-13 19:45:07 +0600

    UIDevice Extension to get device type

---
+++ UIDevice+GetDeviceType.swift
@@ -0,0 +1,44 @@
+//
+//  UIDevice+GetDeviceType.swift
+//  
+//
+//  Created by ankur.lahiry on 13/1/19.
+//
+
+import Foundation
+
+extension UIDevice {
+    
+    var iPhone: Bool {
+        return UIDevice().userInterfaceIdiom == .phone
+    }
+    
+    enum ScreenType: String {
+        case iPhone4
+        case iPhone5
+        case iPhone6
+        case iPhone6Plus
+        case iPhoneX
+        case Unknown
+    }
+    
+    var screenType: ScreenType {
+        guard iPhone else { return .Unknown}
+        switch UIScreen.main.nativeBounds.height {
+        case 960:
+            return .iPhone4
+        case 1136:
+            return .iPhone5
+        case 1334:
+            return .iPhone6
+        case 2208:
+            return .iPhone6Plus
+        case 2436:
+            return .iPhoneX
+        default:
+            return .Unknown
+        }
+    }
+    
+}
+
